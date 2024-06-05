6a54d3ae4fa3da931cabb116d61951777e8287a7
Maroussia Arnault <maroussia.arnault@gmail.com>
2024-06-05 20:37:08 +0200

    refact: clean up

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,7 @@ use grrs::find_matches;
 use grrs::Cli;
 
 fn main() -> std::io::Result<()> {
-    let pb = indicatif::ProgressBar::new(100);
     let args = Cli::parse();
     find_matches(&args, &mut std::io::stdout())?;
-    for i in 0..100 {
-        pb.println(format!("[+] finished #{}", i));
-        pb.inc(1);
-    }
-    pb.finish_with_message("done");
-
     Ok(())
 }
